73eb1fd9d6b8aedb52b723e6ec059b6521ae421d
luwei <luwei@itiger.com>
2020-08-15 08:51:36 +0800

    0815-1

--- src/Practice/class7Practice/IsBalance.java
+++ src/Practice/class7Practice/IsBalance.java
@@ -45,15 +45,37 @@ public class IsBalance {
     return new Info(isBalance, height);
   }
 
+  public static boolean isBalanced2(TreeNode head){
+    return is2(head).isBalance;
+  }
+
+  public static Info is2(TreeNode head){
+    if (head == null){
+      return  new Info(true,0);
+    }
+    Info leftInfo = is2(head.left);
+    Info rightInfo = is2(head.right);
+    int height =Math.max(leftInfo.high,rightInfo.high)+1;
+
+    Boolean isBalance = false;
+    if (leftInfo.isBalance&&rightInfo.isBalance&&Math.abs(leftInfo.high-rightInfo.high)<=1){
+      isBalance = true;
+    }
+    return new Info(isBalance,height);
+  }
+
+
   public static void main(String[] args) {
     TreeNode head = new TreeNode(1);
     head.left = new TreeNode(2);
     head.right = new TreeNode(3);
     head.left.left = new TreeNode(4);
-    head.left.left.left = new TreeNode(5);
+    //head.left.left.left = new TreeNode(5);
     //head.left.right = new TreeNode(5);
     //head.right.left = new TreeNode(6);
     //head.right.right = new TreeNode(7);
     System.out.println(isBalanced(head));
+    System.out.println(isBalanced2(head));
+
   }
 }
